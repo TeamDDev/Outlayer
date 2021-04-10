6de55cf70d40498d3c2b80dbb80bd09a5128890a
ashishkumar <meashish40@gmail.com>
2021-04-10 14:31:28 +0530

    Profile and Records Model Created

--- outlayer/tracker/models.py
+++ outlayer/tracker/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Profile(models.Model):
+    name=models.ForeignKey(User,on_delete=models.CASCADE)
+    monthly_limit=models.FloatField(default=0)
+    expenses_soFar=models.FloatField(default=0)
+
+    def __str__(self):
+        return str(self.name)
+    
+
+
+class Records(models.Model):
+    expenditure_user=models.ForeignKey(User,on_delete=models.CASCADE)
+    expenditure_name=models.CharField(max_length=100)
+    expenditure_type=models.CharField(max_length=100)
+    expenditure_date=models.DateField()
+    expenditure_amount=models.FloatField
+
+    def __str__(self):
+    	return str(self.user)
+    
