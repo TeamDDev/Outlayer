e040a41a198ff884c962fb7452cd172dbf0c598a
nipan09 <pnishant46@gmail.com>
2021-04-19 17:40:58 +0530

    url to home page added

--- outlayer/tracker/urls.py
+++ outlayer/tracker/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('signup', views.signup_view, name='signup'),
-    path('login', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('home/', views.add_view, name='add'),
 ]
