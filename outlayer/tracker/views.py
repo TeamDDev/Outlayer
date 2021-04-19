7f8881e27ac5514713e7bf8fcf249a9868809840
nipan09 <pnishant46@gmail.com>
2021-04-19 17:41:58 +0530

    signup view extended to non-post request

--- outlayer/tracker/views.py
+++ outlayer/tracker/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from tracker.forms import UserForm
+from .models import Profile
 
 def signup_view(request):
     if request.method == 'POST':
         form = UserFrom(request.POST)
         if form.is_valid():
-            form.save()
-        return redirect('login')
+            user = form.save()
+            Profile.objects.create(name='user', monthly_limit=0, expenses_soFar=0)
+            return redirect('login')
+    else:
+        form = UserForm()
+    return render('registeration/signup.html',{'form':form})
+
+
+
