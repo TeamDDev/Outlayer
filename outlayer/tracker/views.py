1b0354c9c0d67fc9b07ce122f580506490e4e1bc
ashishkumar <meashish40@gmail.com>
2021-04-21 14:20:39 +0530

    Login View Created

--- outlayer/tracker/views.py
+++ outlayer/tracker/views.py
@@ -12,6 +12,22 @@ def signup_view(request):
     else:
         form = UserForm()
     return render('registeration/signup.html',{'form':form})
+	
+def log_in view(request):
+       if request.method == 'POST':
+	       form= AuthenticationForm(data=request.POST)
+		   if form.is_valid():
+		           user=form.get_user()
+				   login(request,user)
+				   return redirect('app',username=user.username)
+		else:
+		    form=AuthenticationForm()
+		return render(request, 'registeration/login.html',{'form':form})
+
+def logout_view(request):
+         logout(request)
+         return redirect('login')		 
+
 
 
 
