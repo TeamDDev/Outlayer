711984c5b2469e36d5700cd2ce8f02698a7691ae
nipan09 <pandey.nishant489@gmail.com>
2021-04-26 23:00:11 +0530

    fix: registeration issues fixed

--- outlayer/tracker/admin.py
+++ outlayer/tracker/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-
+from tracker.models import Profile, Records
 # Register your models here.
- models = apps.get_models()
- for model in models:
-     admin.site.register(model)
- 
+'''
+models = apps.get_models()
+for model in models:
+    admin.site.register(model)
+'''
+
+admin.site.register(Profile)
+admin.site.register(Records)
