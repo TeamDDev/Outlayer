1fa68b3d47691d26cee93935c2974c2550734a48
nipan09 <pandey.nishant489@gmail.com>
2021-04-26 23:01:52 +0530

    fix: login_view naming fixed

--- outlayer/tracker/views.py
+++ outlayer/tracker/views.py
@@ -12,17 +12,17 @@ def signup_view(request):
     else:
         form = UserForm()
     return render('registeration/signup.html',{'form':form})
-	
-def log_in view(request):
-       if request.method == 'POST':
-	       form= AuthenticationForm(data=request.POST)
-		   if form.is_valid():
-		           user=form.get_user()
-				   login(request,user)
-				   return redirect('app',username=user.username)
-		else:
-		    form=AuthenticationForm()
-		return render(request, 'registeration/login.html',{'form':form})
+
+def login_view(request):
+        if request.method == 'POST':
+           form= AuthenticationForm(data=request.POST)
+           if form.is_valid():
+               user=form.get_user()
+               login(request,user)
+               return redirect('app',username=user.username)
+        else:
+            form=AuthenticationForm()
+        return render(request, 'registeration/login.html',{'form':form})
 
 def logout_view(request):
          logout(request)
