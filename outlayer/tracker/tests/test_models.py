c22376a035fa44f57665cad87d968a4c02bd4343
nipan09 <pandey.nishant489@gmail.com>
2021-04-26 23:03:57 +0530

    model view test intialized

---
+++ outlayer/tracker/tests/test_models.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+from tracker.models import Profile, Records
+
+class ModelsTestCase(TestCase):
+    def test_user_profile(self):
+        pass
---
+++ outlayer/tracker/tests/test_views.py
@@ -0,0 +1,5 @@
+from django.test import TestCase
+
+class ViewTestCase(TestCase):
+    def test_view(self):
+        pass
