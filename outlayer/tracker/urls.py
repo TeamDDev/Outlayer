80e5126aa820ef1af7071eb2f46b1f08c9c1f4d0
nipan09 <pandey.nishant489@gmail.com>
2021-05-11 20:58:18 +0530

    home url added

--- outlayer/tracker/urls.py
+++ outlayer/tracker/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns=[
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
+    path('home/<username>', views.home_view, name='home'),
 ]
+
+# 
