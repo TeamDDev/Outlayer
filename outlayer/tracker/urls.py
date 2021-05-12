fb7edb0bd71ea72baf0bd37f1fda447f8682a3bd
nipan09 <pandey.nishant489@gmail.com>
2021-05-12 09:10:23 +0530

    profile url added

--- outlayer/tracker/urls.py
+++ outlayer/tracker/urls.py
@@ -5,6 +5,9 @@ urlpatterns=[
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
     path('home/<username>', views.home_view, name='home'),
+    path('add_item', views.additem_view, name='add_item'),
+    path('logout/', views.logout_view, name='logout'),
+    path('profile/', views.profile_view, name='profile'),
 ]
 
 # 
